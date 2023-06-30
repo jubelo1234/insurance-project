e1e5f79302dbbf73df389ed774be2b3109903d7e
jubelo1234 <ohootuberecaiah@gmail.com>
2023-06-30 14:57:35 +0100

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const open_menu = document.querySelector(".ham_imenu");
+const close_menu = document.querySelector(".cancel");
+
+open_menu.addEventListener('click', function () {
+    console.log(`Button clicked`);
+})
