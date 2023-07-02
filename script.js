d6c4f637708759aca7cbd06a863051e851463274
jubelo1234 <ohootuberecaiah@gmail.com>
2023-07-02 13:33:47 +0100

    menu
    good

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 'use strict';
 
-const open_menu = document.querySelector(".ham_imenu");
+const open_menu = document.getElementById("hmbur");
 const close_menu = document.querySelector(".cancel");
+const nav_menu = document.querySelector(".mobil");
+// let query = window.matchMedia("(max-width: 1024px)")
 
 open_menu.addEventListener('click', function () {
-    console.log(`Button clicked`);
+    nav_menu.classList.remove("hidden");
+    // open_menu.classList.toggle("hidden");
+    open_menu.style.display = "none";
+    close_menu.classList.remove("hidden");
+})
+
+close_menu.addEventListener('click', function () {
+    nav_menu.classList.add("hidden");
+    open_menu.style.display = "inline";
+    close_menu.classList.add("hidden");
 })
